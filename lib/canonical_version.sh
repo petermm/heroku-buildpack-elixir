158317f5615f54bfd63c551a047a9933e9f3ff5a
Peter Madsen-mygdal <petermm@gmail.com>
2021-05-26 18:22:08 +0200

    use bob for heroku-18

--- lib/canonical_version.sh
+++ lib/canonical_version.sh
@@ -3,6 +3,8 @@
 erlang_builds_url() {
   if [ "$STACK" = "heroku-20" ]; then
     erlang_builds_url="https://repo.hex.pm/builds/otp/ubuntu-20.04"
+  elif  [ "$STACK" = "heroku-18" ]; then
+    erlang_builds_url="https://repo.hex.pm/builds/otp/ubuntu-18.04"
   else
     erlang_builds_url="https://s3.amazonaws.com/heroku-buildpack-elixir/erlang/cedar-14"
   fi
@@ -12,6 +14,9 @@ erlang_builds_url() {
 fetch_erlang_versions() {
   if [ "$STACK" = "heroku-20" ]; then
     url="https://repo.hex.pm/builds/otp/ubuntu-20.04/builds.txt"
+    curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}'
+  elif  [ "$STACK" = "heroku-18" ]; then
+    url="https://repo.hex.pm/builds/otp/ubuntu-18.04/builds.txt"
     curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}'
   else
     url="https://raw.githubusercontent.com/HashNuke/heroku-buildpack-elixir-otp-builds/master/otp-versions"
